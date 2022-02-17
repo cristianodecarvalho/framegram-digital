af8947ba1fdf54b0127c2d9a05520b7517a071b5
Cristiano de Carvalho <cristiano.filho@dellead.com>
2022-02-17 01:16:26 -0300

    Adicionando docker ao projeto

---
+++ framegram/src/main/resources/db/migration/V002__insert_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_user
+(id, name, login, password, profile)
+VALUES 
+(1, 'Usuário UM', 'usuario01', '$2a$10$COTPia7GD7Ws9TLcAaKWN.dkH4fYWdrqjOK71SsC1JEIkZUQPn/Wm', 'USER'),
+(2, 'Usuário DOIS','usuario02', '$2a$10$cNaWxNjQlt94BprzREK3net04ZLx01ao43CUwWcQdTJN1J52sJKbG', 'USER');
